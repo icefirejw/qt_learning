1a02db8a5c8968f8a2e11be1d18352f875089757
sam <sw_coding@outlook.com>
2020-05-16 17:17:54 +0800

    control introduction finished

---
+++ control02/widget.cpp
@@ -0,0 +1,47 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QMovie>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    //通过按钮切换栈容器的内容(stacked widget)
+    // 切换至 scroll widget
+    connect(ui->btn_scroll,&QPushButton::clicked,[=](){
+        ui->stackedWidget->setCurrentIndex(0);
+    });
+    // 切换至 tool widget
+    connect(ui->btn_tool,&QPushButton::clicked,[=](){
+        ui->stackedWidget->setCurrentIndex(1);
+    });
+    // 切换至 tab widget
+    connect(ui->btn_tab,&QPushButton::clicked,[=](){
+        ui->stackedWidget->setCurrentIndex(2);
+    });
+
+    //通过按钮选择下拉菜单
+    ui->combo_box->addItem(QString::fromLocal8Bit("平民"));
+    ui->combo_box->addItem(QString::fromLocal8Bit("英雄"));
+    ui->combo_box->addItem(QString::fromLocal8Bit("坏蛋"));
+    connect(ui->btn_hero, &QPushButton::clicked, [=](){
+        ui->combo_box->setCurrentIndex(1);
+    });
+
+    //利用label显示图片
+    ui->label_img->setPixmap(QPixmap(":/img/test.png"));
+    //利用label显示动图
+    QMovie *qMovie = new QMovie(":/img/test.gif");
+    ui->label_gif->setMovie(qMovie);
+    //动图需要使用start方法来播放
+    qMovie->start();
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
---
+++ small_widget/smallwidget.cpp
@@ -0,0 +1,32 @@
+﻿#include "smallwidget.h"
+#include "ui_smallwidget.h"
+
+SmallWidget::SmallWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::SmallWidget)
+{
+    ui->setupUi(this);
+
+    //spinbox 值修改的时候 进度条也跟着修改
+    //原信号存在重载，需要定义函数指针明确使用哪一个信号函数
+    void (QSpinBox:: * smallSignal)(int) = &QSpinBox::valueChanged;
+    connect(ui->spinBox,smallSignal, ui->hSlider, &QSlider::setValue);
+
+    //进度条修改的时候 spinbox的值跟着修改
+    connect(ui->hSlider,&QSlider::valueChanged, ui->spinBox, &QSpinBox::setValue);
+}
+
+void SmallWidget::setValue(int v)
+{
+    ui->spinBox->setValue(v);
+}
+
+int SmallWidget::getValue()
+{
+    return ui->spinBox->value();
+}
+
+SmallWidget::~SmallWidget()
+{
+    delete ui;
+}
